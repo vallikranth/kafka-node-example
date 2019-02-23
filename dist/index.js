1b06d19f57d1da30842c460c203f574714ee64c7
Vallikranth <26803438+vallikranth@users.noreply.github.com>
2019-02-23 16:57:27 -0500

    remove ignored files.

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var KafkaProducer_1 = require("./KafkaProducer");
-console.log('Hello Index');
-var producer = new KafkaProducer_1.KafkaProducer();
-producer.sendMessage();
